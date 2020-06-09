f2cc4653194a0c702920ad8a88907bf8da972d65
tln2611 <66456169+tln2611@users.noreply.github.com>
2020-06-08 21:46:46 -0700

    Update PingPong.java

--- src/project/PingPong.java
+++ src/project/PingPong.java
@@ -2,23 +2,19 @@ package project;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class PingPong extends JFrame {
-	static JFrame frame = new JFrame();
-	static final int ROWS = 17, COLS = 29;
-    //private static final int HEIGHT = 500, WIDTH = 800;
-    static int[][] board1 = 
+
+public class PingPong extends JFrame { 
+	
+    static int[][] board = 
 		{
 				{2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
 				{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
@@ -38,14 +34,13 @@ public class PingPong extends JFrame {
 				{2,0,0,0,0,0,0,0,0,0,0,0,0,7,7,7,7,0,0,0,0,0,0,0,0,0,0,0,2},
 				{2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},};
 	
-	public static int[][] display1(int[][] board1) throws IOException {
+	public static int[][] display(int[][] board) throws IOException {
+		JFrame frame = new JFrame();
 		BufferedImage img = ImageIO.read(new File("images.png"));
-		
+		int ROWS = 17;
+		int COLS = 29;
 		frame.setSize(500, 500);
-		frame.setSize(500, 500);
-		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(400, 400));
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);		
 		frame.add(new JComponent() {
 			protected void paintComponent(Graphics g) {
 				Rectangle rec = g.getClipBounds();
@@ -53,13 +48,13 @@ public class PingPong extends JFrame {
 				int h = rec.height/ROWS;
 				for (int i = 0; i < ROWS; i++) {
 					for (int j = 0; j < COLS; j++) {
-						g.drawImage(img, j*w, i*h, j*w+w, i*h+h, board1[i][j]*16, 0, board1[i][j]*16+16, 16, null);
+						g.drawImage(img, j*w, i*h, j*w+w, i*h+h, board[i][j]*16, 0, board[i][j]*16+16, 16, null);
 					}
 				}
 			}
 		});
 		frame.setVisible(true);
-		return board1;		
+		return board;		
 	}
     public PingPong() {
         super("PingPong by Team 5");
@@ -73,7 +68,7 @@ public class PingPong extends JFrame {
     }
 
     public static void main(String[] args) throws IOException {
-    	board1 = display1(board1);
+    	board = display(board);
         new PingPong();
     }
 }
