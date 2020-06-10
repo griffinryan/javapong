54958b83a76c934444cae322094ca7eb8b3f66d9
torpoise <torpoise@icloud.com>
2020-06-09 19:19:30 -0700

    Fixed the glow for final time! Pink!

--- src/project/BouncingBall.java
+++ src/project/BouncingBall.java
@@ -2,12 +2,11 @@ package project;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Random;
 
 public class BouncingBall extends Sprite {
 
     int colorCount = 0; // Set a counter to make color change not flash. Could be boolean.
-    int red = 80;
+    int red = 2;
     int blue = 255;
     int green = 0;
     // Removed color variable. Added color int variables.
@@ -22,10 +21,12 @@ public class BouncingBall extends Sprite {
     }
 
     public void paint(Graphics g) {
-        /* Update color values for glow. || red >= 253 and && red >= 80 && red <= 254*/
+        /*  Update color values for glow.
+            Use if/else statements and a count integer 
+            for setting red value before g.setColor() call.   */
         if(colorCount == 1){
             red = red - 1;
-            if(red == 80){
+            if(red == 2){
                 // Set count equal to 0.
                 colorCount = 0;
             }
