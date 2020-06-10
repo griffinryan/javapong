1f3dc5c841f87b602cd11894f561b66e31e33770
torpoise <torpoise@icloud.com>
2020-06-09 18:38:50 -0700

    Fixed color change again lol.

--- src/project/BouncingBall.java
+++ src/project/BouncingBall.java
@@ -5,7 +5,11 @@ import java.awt.Graphics;
 import java.util.Random;
 
 public class BouncingBall extends Sprite {
-	// Color color = Color.RED;
+
+    int colorCount = 0; // Set a counter to make color change not flash.
+    int red, green, blue;
+    // Removed color variable.
+
     public BouncingBall(PingPong game) {
         super(0, 0, 1, 1, 30, 30, Color.RED);
     }
@@ -16,8 +20,8 @@ public class BouncingBall extends Sprite {
     }
 
     public void paint(Graphics g) {
-    	Random r = new Random();
-    	g.setColor(new Color(155 + r.nextInt(100), 225 + r.nextInt(30), 0)); //random color changing but too fast
+        Random r = new Random();
+        g.setColor(new Color(red, green, blue));
     	// g.setColor(color);
         g.fillOval(getX(), getY(), getWidth(), getHeight());
     }
